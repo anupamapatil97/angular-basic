ceb5ec4fee536c81e0d8a89d6411564a7fedef8f
patilanupama <anupama.patil@yash.com>
2023-04-05 15:21:21 +0530

    data binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  isallowed= false
+  serverCreateStatus="No server is created"
+  serverName="Test"
 
+  constructor(){
+    setTimeout(()=>{
+      this.isallowed=true
+    },2000)
+  }
+  onServerCreation=()=>{
+    this.serverCreateStatus= "Server Created Successfully! Name is " + this.serverName
+  }
+  onServerNameChange=(event:any)=>{
+    this.serverName= event.target.value
+
+  }
 }
